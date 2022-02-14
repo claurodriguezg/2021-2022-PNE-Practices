950c9fe4f3121f866e9ccca040f5cb7f840fa351
claudia <c.rodriguezg.2021@alumnos.urjc.es>
2022-02-14 12:18:29 +0100

    creation of calculate_weight

---
+++ P0/Ex3.py
@@ -0,0 +1,9 @@
+import seq0
+list_genes, result, g = seq_len(seq)
+print("------| Exercise 3 |------")
+if g == "U5":
+    result_U5 = len(result)
+
+print (result_U5)
+
+
--- P0/seq0.py
+++ P0/seq0.py
@@ -18,3 +18,10 @@ def seq_read_fasta(filename):
     seq = open("./sequences/" + filename, "r").read()
     seq = seq[seq.find("\n"):].replace("\n", "")
     return seq
+
+def seq_len(seq):
+    list_genes = ["U5", "ADA", "FRAT1", "FXN"]
+    for g in list_genes:
+        result = seq(g)
+    return list_genes, result, g
+
