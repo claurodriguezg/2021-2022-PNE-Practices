906940aeefeb32adb58b8936d0f7d1a1d7a63a8d
claudia <c.rodriguezg.2021@alumnos.urjc.es>
2022-02-14 11:49:07 +0100

    creation of calculate_weight

--- session - 03/dna_count.py
+++ session - 03/dna_count.py
@@ -4,7 +4,6 @@ count_c = 0
 count_g = 0
 count_t = 0
 for base in chain:
-
     if base == "A":
         count_a += 1
     elif base == "C":
@@ -13,14 +12,10 @@ for base in chain:
         count_g += 1
     elif base == "T":
         count_t += 1
-    else:
-        print("The chain´s bases must be A,C,T or G")
-
 
 print("Total lenght: ", len(chain))
 print("A: ", count_a)
 print("C: ", count_c)
 print("T: ", count_t)
 print("G: ", count_g)
-
 
