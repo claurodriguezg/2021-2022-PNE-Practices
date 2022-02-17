ed8caabf4cd40f4eb28ace22f9951b606f4019d0
claudia <c.rodriguezg.2021@alumnos.urjc.es>
2022-02-17 18:35:57 +0100

    creation of calculate_weight

--- P0/Ex4.py
+++ P0/Ex4.py
@@ -1,10 +1,10 @@
 import seq0
 gene_list = ["U5", "ADA", "FRAT1", "FXN"]
-print("------| Exercise 3 |------")
+print("------| Exercise 4 |------")
 for g in gene_list:
     FOLDER = "./sequences/"
     f = open(FOLDER + g + ".txt", "r").read()
     full_f = f[f.find("\n") + 1:].replace("\n", "")
-    length = seq0.seq_len(gene_list, full_f)
-    print("Gene", g, "----> Length: ", len(full_f))
-
+    length = seq0.seq_count_base(gene_list, full_f):
+    print("Gene", g, ":")
+    print("A: ", a, "\n", "C: ", c, "\n", "T: ", t, "\n", "G: ", g)
--- P0/seq0.py
+++ P0/seq0.py
@@ -19,16 +19,23 @@ def seq_read_fasta(filename):
     seq = seq[seq.find("\n"):].replace("\n", "")
     return seq
 
-def seq_len():
-    gene_list = ["U5", "ADA", "FRAT1", "FXN"]
+def seq_len(gene_list, full_f):
     for g in gene_list:
-        FOLDER = "./sequences/"
-        f = open(FOLDER + g + ".txt", "r").read()
-        full_f = f[f.find("\n") + 1:].replace("\n", "")
         length = len(full_f)
 
+    return length
 
-    return g, length
+def seq_count_base(gene_list, full_f):
+    bases = ["A", "C", "T", "G"]
+    for g in gene_list:
+        a = count(bases[0])
+        c = count(bases[1])
+        t = count(bases[2])
+        g = count(bases[3])
+    return a, c, t, g
+
+
+
 
 
 
