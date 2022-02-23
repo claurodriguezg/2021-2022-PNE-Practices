9dd1fedf0aafcd94e12634c57c48e84a965bc019
claudia <c.rodriguezg.2021@alumnos.urjc.es>
2022-02-23 13:28:34 +0100

    creation of calculate_weight

---
+++ P1/Ex3.py
@@ -0,0 +1,10 @@
+import Seq1
+print("----| Exercise 1, Exercise 3 |-----")
+s1 = Seq1.Seq("")
+s2 = Seq1.Seq("ACTGA")
+s3 = Seq1.Seq("FDAAC")
+
+if s1.null() == True:
+    print(f"Sequence 1: NULL ")
+print(f"Sequence 2: {s2}")
+print(f"Sequence 3: {s3}")
--- P1/Seq1.py
+++ P1/Seq1.py
@@ -1,15 +1,54 @@
 class Seq:
 
-    def __init__(self, strbases):
-        self.strbases = strbases
+    def __init__(self, strbases): #strbasis son los parametros de la sequencia
+        self.strbases = strbases #bases de la sequencia (self --> constructor)
         if not self.valid_sequence():
             self.strbases = "ERROR"
             print("ERROR !!")
         else:
-            print("New sequence created")
+            print("New sequence created !")
+
+    def __init__(self, strbases = "NULL"):
+        self.strbases = strbases  # bases de la sequencia (self --> constructor)
+        if not self.valid_sequence():
+            self.strbases = "ERROR"
+            print("INVALID Seq!")
+
+        elif len(self.strbases) == 0:
+            print("NULL Seq created")
+
+        else:
+            print("New sequence created !")
+
 
 
     def __str__(self):
         """Method called when the object is being printed"""
         return self.strbases
+
+
+    def valid_sequence(self):
+        # add a method to a function
+        # in classes the functions can be written in any order --> they don´t have to be in order to be called one by one
+        valid = True
+        i = 0
+        while i < len(self.strbases) and valid:
+            c = self.strbases[i]
+            if c != "A" and c != "C" and c != "G" and c != "T":
+                valid = False
+            i += 1
+        return valid
+
+    def len(self):
+        """Calculate the length of the sequence"""
+        if not self.strbases == "ERROR":
+            len(self.strbases) == 0
+        return len(self.strbases)
+
+    def null(self):
+        """Check if the length of the sequence is 0"""
+        return len(self.strbases) == 0
+
+
+
 
