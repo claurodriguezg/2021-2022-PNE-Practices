2ac31c0276f2d2d9e1016ab8971b5a91e2c1e311
claudia <c.rodriguezg.2021@alumnos.urjc.es>
2022-03-02 18:38:49 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	P1/Seq1.py

---
+++ P2/Client0.py
@@ -0,0 +1,43 @@
+import socket
+
+
+class Client:
+
+   # PORT = 8000
+    #IP = "localhost"
+
+
+    def __init__(self, ip, port):
+        self.port = port
+        self.ip = ip
+
+    def ping(self):
+        print("OK")
+
+    def __str__(self):
+
+        return "Connection to SERVER at "  +  str(self.ip) + " PORT:" + str(self.port)
+
+    def talk(self, msg):
+        # -- Create the socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # establish the connection to the Server (IP, PORT)
+        s.connect((self.ip, self.port))
+
+        # Send data.
+        s.send(str.encode (msg))
+
+        # Receive data
+        response = s.recv(2048).decode("utf-8")
+
+        # Close the socket
+        s.close()
+
+        # Return the response
+        return response
+
+
+
+
+
---
+++ P2/Server.py
@@ -0,0 +1,47 @@
+import socket
+
+PORT = 9000
+IP = "127.0.0.1"
+MAX_OPEN_REQUESTS = 5
+
+# Counting the number of connections
+number_con = 0
+
+# create an INET, STREAMing socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    serversocket.bind((IP, PORT))
+    # become a server socket
+    # MAX_OPEN_REQUESTS connect requests before refusing outside connections
+    serversocket.listen(MAX_OPEN_REQUESTS)
+
+    while True:
+        # accept connections from outside
+        print("Waiting for connections at {}, {} ".format(IP, PORT))
+        (clientsocket, address) = serversocket.accept() #this is just for the person to conect
+        #if someone connects no one else can
+
+        # Another connection!e
+        number_con += 1
+
+        # Print the conection number
+        print("CONNECTION: {}. From the IP: {}".format(number_con, address))
+
+        # Read the message from the client, if any
+        msg = clientsocket.recv(2048).decode("utf-8")
+        print("Message from client: {}".format(msg))
+
+        # Send the messag
+        message = "Hello from the teacher's server"
+        #message = len(message) #---> msg = message
+        send_bytes = str.encode(message) #we can nor apply the encode to a int --> send_bytes = str(message).encode()
+        # We must write bytes, not a string
+        clientsocket.send(send_bytes)
+        clientsocket.close() #the fist user leaves the server. A new user can connect now
+
+except socket.error:
+    print("Problems using port {}. Do you have permission?".format(PORT))
+
+except KeyboardInterrupt:
+    print("Server stopped by the user")
+    serversocket.close()
