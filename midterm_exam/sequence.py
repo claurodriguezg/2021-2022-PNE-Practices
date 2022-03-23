e89939b790c3280a1faeb9bcf9f4f90f7effa746
claudia <c.rodriguezg.2021@alumnos.urjc.es>
2022-03-23 18:31:09 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	P1/Seq1.py

---
+++ midterm_exam/sequence.py
@@ -0,0 +1,140 @@
+class Seq:
+
+    def __init__(self, strbases = "NULL"):  #strbasis son los parametros de la sequencia
+        self.strbases = strbases  # bases de la sequencia (self --> constructor)
+
+        if strbases == "NULL":
+            print("NULL Seq created !")
+
+        elif not self.valid_sequence():
+            self.strbases = "ERROR"
+            print("INVALID Seq!")
+
+        else:
+            self.strbases = strbases
+            print("New sequence created !")
+
+    def __str__(self):
+        """Method called when the object is being printed"""
+        return self.strbases
+
+
+    def valid_sequence(self):
+        # add a method to a function
+        # in classes the functions can be written in any order --> they don´t have to be in order to be called one by one
+        valid = True
+        i = 0
+        while i < len(self.strbases) and valid:
+            c = self.strbases[i]
+            if c != "A" and c != "C" and c != "G" and c != "T":
+                valid = False
+            i += 1
+        return valid
+
+    def len(self):
+        """Calculate the length of the sequence"""
+        new_len = ""
+        if self.strbases == "ERROR" or self.strbases == "NULL":
+            new_len = 0
+            return(new_len)
+        else:
+            return len(self.strbases)
+
+
+    def seq_read_fasta(self,arg):
+        f = open("../P0/sequences/" + arg + ".txt",  "r").read()
+        self.strbases = f[f.find("\n"):].replace("\n", "")
+
+
+    def count_base(self):
+        count_a = 0
+        count_c = 0
+        count_g = 0
+        count_t = 0
+        if len(self.strbases) > 0:
+            count_a += self.strbases.count("A")
+            count_c += self.strbases.count("C")
+            count_g += self.strbases.count("G")
+            count_t += self.strbases.count("T")
+
+        return count_a, count_c, count_g, count_t
+
+    def count(self):
+        bases = ["A", "C", "G", "T"]
+        count = []
+        a = count.append(self.strbases.count("A"))
+        c = count.append(self.strbases.count("C"))
+        g = count.append(self.strbases.count("G"))
+        t = count.append(self.strbases.count("T"))
+        dict_six = dict(zip(bases, count))
+
+        return a, c, g, t, count, dict_six
+
+    def reverse(self):
+        if self.valid_sequence():
+            reversed_str = self.strbases[len(self.strbases) - 1 :: -1 ]
+        elif not self.valid_sequence():
+            reversed_str = self.strbases
+
+        if len(reversed_str) == 0:
+            reversed_str = "NULL"
+
+        return reversed_str
+
+    def complement(self,arg):
+        compl = ""
+        for g in arg:
+            if g == "A":
+                compl += "T"
+            elif g == "T":
+                compl += "A"
+            elif g == "C":
+                compl += "G"
+            elif g == "G":
+                compl += "C"
+        if len(compl) == 0:
+            compl = "NULL"
+        if self.strbases == "ERROR":
+            compl = "ERROR"
+        return compl
+
+    def read_fasta(self, filename):
+        try:
+            full_seq = open(filename, "r").read()
+            full_seq = full_seq[full_seq.find("\n"):].replace("\n", "")
+            return full_seq
+
+        except FileNotFoundError:
+            print("File does not exist. Choose another file.")
+
+    def genes_bases(self):
+        bases = ["A", "C", "G", "T"]
+        count_a, count_c, count_g, count_t = self.count_base()
+        total = [count_a, count_c, count_g, count_t]
+        d = dict(zip(total, bases))
+        ordered = max(d)
+
+        return ordered, d
+
+    def bases(self, arg):
+        bases = ["A", "C", "G", "T"]
+        count_a, count_c, count_g, count_t = self.count_base()
+        total = [count_a, count_c, count_g, count_t]
+        d = dict(zip(total, bases))
+        message = ""
+        for count, base in d.items():
+            if base == "A":
+                a = count
+            elif base == "C":
+                c = count
+            elif base == "G":
+                g = count
+            elif base == "T":
+                t = count
+            message += base + ":" + str(count) +  " " + "(" + str(round((count*100)/len(arg), 1)) + "%)" + "\n"
+
+        return message,count
+
+
+
+
---
+++ midterm_exam/server.py
@@ -0,0 +1,160 @@
+import socket
+import colorama
+import sequence
+
+# Configure the Server's IP and PORT
+PORT = 8080
+IP = "127.0.0.1"
+
+# -- Step 1: create the socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# -- Optional: This is for avoiding the problem of Port already in use
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  #addresses can be reused
+#run two servers with the same port --> avoid adress already used error.
+
+# -- Step 2: Bind the socket to server's IP and PORT
+ls.bind((IP, PORT))
+
+# -- Step 3: Configure the socket for listening
+ls.listen()  #if we dont specified the operating system will handle by itself how any clients can be at the same time
+
+print("SEQ Server is configured!")
+
+while True:
+    # -- Waits for a client to connect
+    print("Waiting for Clients to connect...")
+    try:
+        (cs, client_ip_port) = ls.accept()
+        # -- Server stopped manually
+    except KeyboardInterrupt:
+        print("Server stopped by the user")
+
+        # -- Close the listening socket
+        ls.close()
+
+        # -- Exit!
+        exit()
+
+    # -- Execute this part if there are no errors
+
+    else:
+
+        print("A client has connected to the server!")
+
+        # -- Read the message from the client
+        # -- The received message is in raw bytes
+        msg_raw = cs.recv(2048)
+
+        # -- We decode it for converting it
+        # -- into a human-redeable string
+        msg = msg_raw.decode().replace("\n", "").strip() #strip para quitar los espacios de la izq y de la derecha
+        splitted_command = msg.split(" ")
+        cmd = splitted_command[0]
+        #arg = msg.split(' ')[1] #if we are not using the msg again we can just do the slipt after the strip
+        if cmd != "PING":
+            arg = splitted_command[1]
+            #print(arg)
+        #print(cmd)
+
+        # -- Print the received message
+        #print(f"Message received: {msg}")
+
+        # -- Send a response message to the client
+        if msg == "PING":
+            color_txt = "PING command! "
+            #print(color_txt)
+            colorama.init()
+            print(colorama.Fore.GREEN + color_txt + colorama.Fore.WHITE)
+            response = "OK!\n"
+
+        elif cmd == "GET":
+            color_txt = "GET"
+            colorama.init()
+            print(colorama.Fore.GREEN + color_txt + colorama.Fore.WHITE)
+            list_seq = ["U5", "FRAT1", "ADA", "FXN", "RNU6_269P"]
+            i = int(arg)
+            if i <= 4:
+                choosen = list_seq[i]
+                seq = sequence.Seq()
+                filename = f"./sequences/" + choosen + ".txt"
+                full_f = str(seq.read_fasta(filename))
+                response = full_f
+
+        elif cmd == "INFO":
+            color_txt = "INFO"
+            colorama.init()
+            print(colorama.Fore.GREEN + color_txt + colorama.Fore.WHITE)
+            seq = sequence.Seq(arg)
+            response, count = seq.bases(arg)
+            #response = seq.convert_message(bases)
+            response = "Sequence: " + arg + "\n" + "Total Length: " + str(len(arg)) + "\n" + response
+
+            #response += " " + "(" + str(round((count*100)/len(arg), 3)) + "%)"
+
+        elif cmd == "COMP":
+            color_txt = "COMP"
+            colorama.init()
+            print(colorama.Fore.GREEN + color_txt + colorama.Fore.WHITE)
+            seq = sequence.Seq(arg)
+            compl = seq.complement(arg)
+            response = compl + " "
+
+        elif cmd == "REV":
+            color_txt = "REV "
+            colorama.init()
+            print(colorama.Fore.GREEN + color_txt + colorama.Fore.WHITE)
+            response = arg[::-1] + " "
+            print("New sequence created!")
+
+        elif cmd == "GENE":
+            color_txt = "GENE "
+            colorama.init()
+            print(colorama.Fore.GREEN + color_txt + colorama.Fore.WHITE)
+
+            seq = sequence.Seq(arg)
+            #s = "../P0/sequences/" + arg + ".txt"
+            #filename = seq.read_fasta(arg)
+            filename = f"./sequences/" + arg + ".txt"
+            full_f = seq.read_fasta(filename)
+            response =  str(full_f)
+
+        elif cmd == "OPE":
+            color_txt = "GENE "
+            colorama.init()
+            print(colorama.Fore.GREEN + color_txt + colorama.Fore.WHITE)
+            a = 0
+            c = 0
+            g = 0
+            t = 0
+            seq = sequence.Seq(arg)
+            #seq.valid_sequence()
+
+            if seq.valid_sequence():
+                for i in arg:
+                    if i == "A":
+                        a += 4
+                    elif i == "C":
+                        c += -3
+                    elif i == "G":
+                        g += 7
+                    elif i == "T":
+                        t += -6
+                response = str(a + c + g + t) + " "
+
+            elif seq == "ERROR" or "NULL":
+                response = "We could not sum the bases since the sequence is not correct."
+
+        else:
+
+            response = "HELLO. I am the Server :-)\n"
+
+        print(response)
+
+
+
+        # -- The message has to be encoded into bytes
+        cs.send(response.encode())
+
+        # -- Close the data socket
+        cs.close()
+
